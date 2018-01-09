4819089de186202ef75cda52474023beb9f53350
David Peppel <skeltonath@gmail.com>
2018-01-09 20:23:33 +0000

    Added eslint using airbnb as base config

--- schreef.js
+++ schreef.js
@@ -1,17 +1,17 @@
-const _       = require('lodash');
+const _ = require('lodash');
 const Discord = require('discord.js');
-const path    = require('path');
-const fs      = require('fs');
-const log4js  = require('log4js');
-const format  = require('format');
+const path = require('path');
+const fs = require('fs');
+const log4js = require('log4js');
+const format = require('format');
 const express = require('express');
-const http    = require('http');
+const http = require('http');
 
 // configure express
 const app = express();
 const port = process.env.PORT || 5000;
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 app.get('/', (req, res) => res.render('index'));
 app.listen(port);
 
@@ -28,35 +28,35 @@ const handlerMap = {};
 // Functions
 function loadHandlers() {
   const handlerPath = path.join(__dirname, 'handlers');
-  const files = _.filter(fs.readdirSync(handlerPath), function(fileName) {
-    return _.endsWith(fileName, '.js');
-  });
+  const files = _.filter(fs.readdirSync(handlerPath), fileName => _.endsWith(fileName, '.js'));
 
-  _.each(files, function(file) {
-    handler = require('./handlers/' + file);
+  _.each(files, (file) => {
+    /* eslint-disable global-require, import/no-dynamic-require */
+    const handler = require(`./handlers/${file}`);
     handlerMap[handler.command] = handler.handler;
-    LOG.info('Loaded ' + handler.name);
+    LOG.info(`Loaded ${handler.name}`);
+    /* eslint-enable global-require, import/no-dynamic-require */
   });
-};
+}
 
 function handleMessage(message) {
   // if message is from bot, skip
   if (message.author.id === client.user.id) return;
 
   // handle command
-  var args = message.content.match(/^\.(\w+)\s*(.*)$/);
+  const args = message.content.match(/^\.(\w+)\s*(.*)$/);
 
   if (!_.isNull(args)) {
-    var command = args[1];
-    var params = args[2];
+    const command = args[1];
+    const params = args[2];
 
     if (_.has(handlerMap, command)) {
       LOG.info(format('Executing %s command', command));
-      var handler = handlerMap[command];
+      const handler = handlerMap[command];
       handler(message.channel, message, params);
     }
   }
-};
+}
 
 // Load handlers, start client, add handlers
 loadHandlers();

202d331dede8c4258506cbb490e010c8e8d4161d
Robert <the.kanbo@gmail.com>
2018-01-10 11:41:13 -0800

    New dad joke function (#12)
    * New dad joke function
    
    * error logging, removing unused requires, changing comment to match

---
+++ handlers/dad.js
@@ -0,0 +1,38 @@
+const rp      = require('request-promise');
+const log4js  = require('log4js');
+
+const LOG = log4js.getLogger('dad');
+
+/**
+ * Retrieving a random dad joke from https://icanhazdadjoke.com/
+ * via their public API
+ */
+module.exports = {
+  name: 'dad',
+  command: 'dad',
+  handler: dad
+};
+
+const DAD_API = 'https://icanhazdadjoke.com/';
+
+function dad(channel, message, params) {
+
+let options = {
+    uri: DAD_API,
+    json: true
+  };
+
+  rp.get(options)
+    .then(joke => {
+      if(joke.joke){
+        channel.send(joke.joke);
+      } else {
+        LOG.error(joke);
+        channel.send('Error receiving joke from Dad Central Station');
+      }
+    })
+    .catch(err =>{
+      LOG.error(err);
+      channel.send('Error receiving joke from Dad Central Station');
+    });
+}

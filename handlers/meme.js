ba923aa93c1c9d887e8bdbdf95572c1367356736
Robert Harvey <robert@efellemedia.com>
2018-01-10 08:32:10 -0800

    removing debug logging, changing some variable names

--- handlers/meme.js
+++ handlers/meme.js
@@ -20,8 +20,8 @@ const MEME_PASSWORD = process.env.MEME_PASSWORD;
 const API_URL = 'http://version1.api.memegenerator.net//Instance_Create';
 const languageCode = 'en';
 let generatorID = 45;
-let text0 = 'GOOD';
-let text1 = 'SHIT';
+let top_text = 'GOOD';
+let bottom_text = 'SHIT';
 function meme(channel, message, params) {
 
   if(!MEME_USER){
@@ -40,11 +40,8 @@ function meme(channel, message, params) {
 
   channel.fetchMessages({ before: message.id})
     .then(messages => {
-      text0 = findMessage(channel, messages).content;
-      // text0 = messages.random().content;
-      text1 = findMessage(channel, messages).content;
-      LOG.info('Top text: ' + text0);
-      LOG.info('Bottom text: ' + text1);
+      top_text = findMessage(channel, messages).content;
+      bottom_text = findMessage(channel, messages).content;
     })
     .catch(error => {
       LOG.error(error);
@@ -57,9 +54,8 @@ function meme(channel, message, params) {
   };
 
   rp.get(op).then(generators => {
-    LOG.info(generators);
     generatorID = _.sample(generators.result).generatorID;
-    let uri = `${API_URL}?apiKey=${MEMEGENERATOR_API_KEY}&generatorID=${generatorID}&languageCode=${languageCode}&text0=${text0}&text1=${text1}&username=${MEME_USER}&password=${MEME_PASSWORD}`;
+    let uri = `${API_URL}?apiKey=${MEMEGENERATOR_API_KEY}&generatorID=${generatorID}&languageCode=${languageCode}&text0=${top_text}&text1=${bottom_text}&username=${MEME_USER}&password=${MEME_PASSWORD}`;
     let options = {
       uri: uri,
       json: true
@@ -72,23 +68,20 @@ function meme(channel, message, params) {
       .catch(error =>{
         LOG.error(error);
         channel.send("Encountered an error while connecting to the world meme database; we've been set up!");
-        channel.send(_.toUpper(text0));
-        channel.send(_.toUpper(text1));
+        channel.send(_.toUpper(top_text));
+        channel.send(_.toUpper(bottom_text));
       });
   })
   .catch(error => {
     LOG.error(error);
     channel.send("Error retrieving meme generators from Memelord, Eternal Ruler of Heaven, Earth and the Interwebz, and All Creatures Who Crawl Upon It, Past, Present and Future, In This and Any Other Dimension");
-    channel.send(_.toUpper(text0));
-    channel.send(_.toUpper(text1));
+    channel.send(_.toUpper(top_text));
+    channel.send(_.toUpper(bottom_text));
   });
 }
 
 function findMessage(channel, messages){
   let found = messages.random();
-  LOG.info(channel.client.user.id);
-  LOG.info(found.author.id);
-  LOG.info(found.content);
   if(found.author.id == channel.client.user.id || found.content.startsWith('.')){
     found = findMessage(channel, messages);
   }

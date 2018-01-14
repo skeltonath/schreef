91085afed84dafa89ca2c626c3131ad1cb6e2aad
Garrett <noideas4thisname@users.noreply.github.com>
2018-01-14 04:38:48 -0800

    Linting fixes and additional cleanup

--- handlers/stock.js
+++ handlers/stock.js
@@ -21,93 +21,122 @@ module.exports = {
 };
 
 function stock(channel, message, params) {
-  // Initialize table service
-  const tableSvc = azure.createTableService(process.env.AZURE_STORAGE_CONNECTION_STRING);
-  const splitParams = params.split(' ');
+    
+    // Ignore bots
+    if(message.author.bot) return;
 
-  // Verify the Azure Storage Table exists and create if it doesn't
-  tableSvc.createTableIfNotExists(TABLENAME, (error, result, response) => {
-    if (!error) {
-      // Table exists or created
-    } else {
-      LOG.error(error);
-    }
-  });
+    // Initialize table service
+    const tableSvc = azure.createTableService(process.env.AZURE_STORAGE_CONNECTION_STRING);
+    let splitParams = params.split(' ');
 
-  // Verify all users have an account and create new ones for those that don't
-  channel.guild.members.forEach((member) => {
-    getStock(tableSvc, member.user.username, (error, result, response) => {
-      if (typeof result !== 'number') {
-        LOG.info(`Setting up new $chreef $tock account for ${member.user.username}`);
-        updateUser(tableSvc, member.user.username, STARTINGSTOCK);
-      }
-    });
-  });
-
-  if (splitParams[0] === 'help') {
-    channel.send('Command Usage:' + '\n' +
-            '.reset - Resets you back to 1000$$' + '\n' +
-            '.balance - Displays your balance' + '\n' +
-            '.allbalances - Displays everyone\'s balance' + '\n' +
-            '.send [username] [amount] - sends user amount of $$' + '\n');
-  }
-
-  // Reset command to go back to starting stock
-  if (splitParams[0] === 'reset') {
-    updateUser(tableSvc, message.author.username, STARTINGSTOCK);
-    LOG.info(`${message.author.username} reset his $$ account to starting value:${STARTINGSTOCK}`);
-  }
-
-  // Get the stock for that user
-  if (splitParams[0] === 'balance') {
-    getStock(tableSvc, message.author.username, (error, result, response) => {
-      channel.send(`${message.author.username}\'s balance is ${result}$$`);
-    });
-  }
-
-  // Show the stock for all users
-  if (splitParams[0] === 'allbalances') {
-    showAllStock(tableSvc, channel);
-  }
-
-  // Send stock to another user if they have an account
-  if (splitParams[0] === 'send') {
-    const recipient = splitParams[1];
-    const sendAmount = parseInt(splitParams[2]);
-
-    if (recipient !== message.author.username) {
-      getStock(tableSvc, recipient, (error, receiverStock, response) => {
-        if (typeof receiverStock === 'number') {
-          getStock(tableSvc, message.author.username, (error, senderStock, response) => {
-            if ((senderStock - sendAmount) > 0 && typeof senderStock === 'number') {
-              const newSenderTotal = senderStock - sendAmount;
-              const newReceiverTotal = receiverStock + sendAmount;
-              let transactionString = 'Transaction Complete:' + '\n';
-              const transactionLog = `${message.author.username} sent ${recipient} ${sendAmount}$$`;
-
-              updateUser(tableSvc, message.author.username, newSenderTotal);
-              updateUser(tableSvc, recipient, newReceiverTotal);
-
-              transactionString += `${message.author.username}: ${senderStock} - ${sendAmount} = ${newSenderTotal}\n${recipient}: ${receiverStock} + ${sendAmount} = ${newReceiverTotal}`;
-
-              LOG.info(transactionLog);
-              channel.send(transactionString);
-            } else {
-              channel.send('Not enough stock to make the transaction.');
-            }
-          });
-        } else {
-          channel.send('That user does not exist! Schreef won\'t throw his stock to the abyss.');
+    // Verify the Azure Storage Table exists and create if it doesn't
+    tableSvc.createTableIfNotExists(TABLENAME, function (error, result, response) {
+        if (!error) {
+            // Table exists or created
         }
-      });
-    } else {
-      channel.send('You can\'t send yourself stock here.');
+        else {
+            LOG.error(error);
+        }
+    });
+
+    // Verify all users have an account and create new ones for those that don't
+    message.client.users.forEach(member => {
+        if (!member.bot) {
+            getStock(tableSvc, member.username, function (error, result, response) {
+                if (typeof result !== 'number') {
+                    LOG.info('Setting up new $chreef $tock account for ' + member.username);
+                    updateUser(tableSvc, member.username, STARTINGSTOCK);
+                }
+            });
+        }
+    });
+
+    if (splitParams[0] === 'help') {
+        message.author.send('.stock [params] Command Usage:' + '\n' +
+            'reset - Resets you back to 1000$$.' + '\n' +
+            'balance [here] - Displays your balance. DM by default, to channel with \"here\".' + '\n' +
+            'allbalances [here] - Displays everyone\'s balance. DM by default, to channel with \"here\".' + '\n' +
+            'send [username] [amount] - sends user amount of $$.' + '\n');
     }
-  }
+
+    // Reset command to go back to starting stock
+    if (splitParams[0] === 'reset') {
+        updateUser(tableSvc, message.author.username, STARTINGSTOCK);
+        LOG.info(`${message.author.username} reset his $$ account to starting value:${STARTINGSTOCK}`);
+    }
+
+    // Get the stock for that user
+    if (splitParams[0] === 'balance') {
+        getStock(tableSvc, message.author.username, function (error, result, response) {
+
+            let balanceString = `${message.author.username}\'s balance is ${result}$$`;
+
+            if(splitParams[1] === 'here' && channel !== null) {
+                channel.send(balanceString);
+            }
+            else {
+                message.author.send(balanceString);
+            }
+        });
+    }
+
+    // Show the stock for all users
+    if (splitParams[0] === 'allbalances') {
+        if(splitParams[1] === 'here') {
+            showAllStock(tableSvc, channel);
+        }
+        else {
+            showAllStock(tableSvc, message.author);
+        }
+    }
+
+    // Send stock to another user if they have an account
+    if (splitParams[0] === 'send') {
+        const recipient = splitParams[1];
+        const sendAmount = parseInt(splitParams[2]);
+
+        if (sendAmount < 0) {
+            channel.send('You can\'t steal $$ here, fleshbag.');
+        }
+        else {
+            if (recipient !== message.author.username) {
+                getStock(tableSvc, recipient, function (error, receiverStock, response) {
+                    if (typeof receiverStock === 'number') {
+                        getStock(tableSvc, message.author.username, function (error, senderStock, response) {
+                            if ((senderStock - parseInt(sendAmount)) > 0 && typeof senderStock === 'number') {
+
+                                const newSenderTotal = senderStock - parseInt(sendAmount);
+                                const newReceiverTotal = receiverStock + parseInt(sendAmount);
+                                let transactionString = 'Transaction Complete:' + '\n';
+                                let transactionLog = `${message.author.username} sent ${recipient} ${sendAmount}$$`;
+
+                                updateUser(tableSvc, message.author.username, newSenderTotal);
+                                updateUser(tableSvc, recipient, newReceiverTotal);
+
+                                transactionString += `${message.author.username}: ${senderStock} - ${sendAmount} = ${newSenderTotal}\n${recipient}: ${receiverStock} + ${sendAmount} = ${newReceiverTotal}`
+
+                                LOG.info(transactionLog);
+                                channel.send(transactionString);
+                            }
+                            else {
+                                channel.send('Not enough $$ to make the transaction. Get a job.')
+                            }
+                        });
+                    }
+                    else {
+                        channel.send('Nobody of that name exists or they\'re a bot. Schreef won\'t throw his $$ to the abyss or filthy machines.');
+                    }
+                });
+            } else {
+                channel.send('You can\'t send yourself stock. Who do you think you are?');
+            }
+        }
+    }
 }
 
 // Used to update or create a user entry in the Azure Table
 function updateUser(tableSvc, username, amount) {
+
   const userEntry = {
     PartitionKey: ENTGEN.String(username),
     RowKey: ENTGEN.String('1'),
@@ -148,19 +177,21 @@ function showAllStock(tableSvc, channel) {
 
 // Gets the amount of stock the user has and makes it available in the callback
 function getStock(tableSvc, username, callback) {
-  const query = new azure.TableQuery().select(['schreefstock']).where('PartitionKey eq ?', username);
+    const query = new azure.TableQuery().select(['schreefstock']).where('PartitionKey eq ?', username);
 
-  tableSvc.queryEntities(TABLENAME, query, null, (error, result, response) => {
-    if (!error) {
-      // query successful
-      if (result.entries.length > 0) {
-        callback(error, result.entries[0].schreefstock._, response);
-      } else {
-        callback(error, null, response);
-      }
-    } else {
-      LOG.error(error);
-      callback(error, null, response);
-    }
-  });
-}
+    tableSvc.queryEntities(TABLENAME, query, null, function (error, result, response) {
+        if (!error) {
+            // query successful
+            if (result.entries.length > 0) {
+                callback(error, parseInt(result.entries[0].schreefstock._), response);
+            }
+            else {
+                callback(error, null, response);
+            }
+        }
+        else {
+            LOG.error(error);
+            callback(error, null, response);
+        }
+    });
+};

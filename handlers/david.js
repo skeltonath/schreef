05aed690165e6182ebc07547006442fc95f3b0c6
Rylie Nelson <ryliemn@gmail.com>
2018-03-03 21:38:08 -0800

    utilize more lodash

--- handlers/david.js
+++ handlers/david.js
@@ -1,5 +1,4 @@
 const _      = require('lodash');
-const format = require('format');
 const helpers = require('../util/helpers.js');
 
 module.exports = {
@@ -17,13 +16,21 @@ const config = {
   removeCharChance: .2,
 }
 
+const modifiers = [
+  w => w,
+  scramble,
+  addChars,
+  removeChars,
+  capitalizationSwap
+];
+
 async function david(message) {
   const messages = await helpers.getMessages(message);
   const randomMessage = helpers.randomUserMessage(messages).content;
   let words = randomMessage.split(" ");
 
   words = _.map(words, (word) => {
-    const fn = getRandomModifier();
+    const fn = _.sample(modifiers);
     return fn(word);
   });
 
@@ -32,24 +39,9 @@ async function david(message) {
   message.channel.send("David: " + davidifiedString);
 }
 
-function getRandomModifier() {
-  const random = Math.random();
-  if (random < .2) {
-    return w => w;
-  } else if (random >= .2 && random < .4) {
-    return scramble;
-  } else if (random >= .4 && random < .6) {
-    return addChars;
-  } else if (random >= .6 && random < .8) {
-    return removeChars;
-  } else if (random >= .8 && random < 1) {
-    return capitalizationSwap;
-  }
-}
-
 // randomly swaps position of two adjacent characters
 function scramble(word) {
-  helpers.debug("Scrambling word: " + word);
+  helpers.debug(`Scrambling words: ${word}`);
   let newWord = "";
   for (let i = 0; i < word.length - 1; i += 2) {
     let random = Math.random();
@@ -60,37 +52,37 @@ function scramble(word) {
       newWord += word.substring(i, i + 2);
     }
   }
-  helpers.debug("Result: " + newWord);
+  helpers.debug(`Result: ${newWord}`);
   return newWord;
 }
 
 function addChars(word) {
-  helpers.debug("Addings chars to word: " + word);
+  helpers.debug(`Adding chars to word: ${word}`);
   let newWord = "";
   for (let i = 0; i < word.length; i++) {
     let random = Math.random();
     newWord += word.charAt(i);
     if (random < config.addCharChance) {
-      newWord += getRandomChar();
+      newWord += String.fromCharCode(_.random(33, 126));
     }
   }
-  helpers.debug("Result: " + newWord);
+  helpers.debug(`Result: ${newWord}`);
   return newWord;
 }
 
 function removeChars(word) {
-  helpers.debug("Removing chars from word: " + word);
+  helpers.debug(`Removing chars from word: ${word}`);
   let newWord = "";
   for (let i = 0; i < word.length; i++) {
     let random = Math.random();
     newWord += random < config.removeCharChance ? "" : word.charAt(i);
   }
-  helpers.debug("Result: " + newWord);
+  helpers.debug(`Result: ${newWord}`);
   return newWord;
 }
 
 function capitalizationSwap(word) {
-  helpers.debug("Capitalization swapping word: " + word);
+  helpers.debug(`Capitalization swapping word: ${word}`);
   let newWord = "";
   for (let i = 0; i < word.length; i++) {
     let random = Math.random();
@@ -104,13 +96,6 @@ function capitalizationSwap(word) {
       newWord += curChar;
     }
   }
-  helpers.debug("Result: " + newWord);
+  helpers.debug(`Result: ${newWord}`);
   return newWord;
-}
-
-function getRandomChar() {
-  const min = Math.ceil(33);
-  const max = Math.floor(127);
-  const charCode = Math.floor(Math.random() * (max - min)) + min;
-  return String.fromCharCode(charCode);
 }

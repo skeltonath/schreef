5a240b1198e139c12911a80babb19c67914b130c
Bradley Stafford <bradleypstafford@gmail.com>
2019-04-13 18:41:08 -0700

    Clean up dad module because I'm coding on a saturday

--- handlers/dad.js
+++ handlers/dad.js
@@ -1,35 +1,28 @@
-const rp = require('request-promise');
+const get = require('request-promise');
 const log4js = require('log4js');
 
-const LOG = log4js.getLogger('dad');
+const log = log4js.getLogger('dad');
 
-/**
- * Retrieving a random dad joke from https://icanhazdadjoke.com/
- * via their public API
- */
+const handler = async ({ channel }) => {
+  // An attempt will be made
+  try {
+    const { joke } = await get({ 
+      uri: 'https://icanhazdadjoke.com',
+      json: true,
+    });
+
+    // IT'S FATHER TIME
+    channel.send(joke);
+  } catch (error) {
+    // An attempt was made
+    log.error(error);
+
+    channel.send('Error receiving joke from Dad Central Station');
+  }
+};
+
 module.exports = {
   name: 'dad',
   trigger: '.dad',
-  handler: dad,
+  handler,
 };
-
-const DAD_API = 'https://icanhazdadjoke.com/';
-
-function dad(message) {
-  const options = {
-    uri: DAD_API,
-    json: true,
-  };
-
-  rp.get(options).then((joke) => {
-    if (joke.joke) {
-      message.channel.send(joke.joke);
-    } else {
-      LOG.error(joke);
-      message.channel.send('Error receiving joke from Dad Central Station');
-    }
-  }).catch((err) => {
-    LOG.error(err);
-    message.channel.send('Error receiving joke from Dad Central Station');
-  });
-}
